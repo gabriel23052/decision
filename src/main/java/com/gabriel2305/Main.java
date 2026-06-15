050b9ec0721eb5e74b4d52f22d628893df311fc0
Gabriel <gabrielggp2002@gmail.com>
2026-06-15 15:10:36 -0300

    feat: first commit

---
+++ src/main/java/com/gabriel2305/Main.java
@@ -0,0 +1,12 @@
+package com.gabriel2305;
+
+import java.util.Scanner;
+
+public class Main {
+    void main() {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        IO.println("Hello " + name);
+    }
+}
+
