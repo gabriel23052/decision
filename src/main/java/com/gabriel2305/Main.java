a06a005bd34bd66613f8c0b3bb7e67d0cbe14c70
Gabriel <gabrielggp2002@gmail.com>
2026-06-19 16:00:15 -0300

    feat: add confirmation when exiting the application

--- src/main/java/com/gabriel2305/Main.java
+++ src/main/java/com/gabriel2305/Main.java
@@ -1,12 +1,15 @@
 package com.gabriel2305;
 
+import java.util.Scanner;
+
 public class Main {
     void main() {
         try {
-            UI.log("Starting application");
             Application application = new Application();
             application.start();
-            UI.log("Finishing application");
+            System.out.println();
+            System.out.println("Pressione Enter para sair...");
+            new Scanner(System.in).nextLine();
         } catch (Exception e) {
             UI.error("Ocorreu uma falha inesperada: " + e.getMessage());
             e.printStackTrace();
